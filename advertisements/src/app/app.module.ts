9bde440d3f10407cc79c0bf820ce60b272529842
Tedo74 <teddy2@abv.bg>
2020-01-18 15:06:09 +0200

    Created Home and Auth components, added router module by hand

--- advertisements/src/app/app.module.ts
+++ advertisements/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavigationComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NavigationComponent,
+		HomeComponent,
+		RegisterComponent,
+		LoginComponent
+	],
+	imports: [ BrowserModule, AppRoutingModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
