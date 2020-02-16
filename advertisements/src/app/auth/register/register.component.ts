d2ee95fe7c2007db85cc2572b8d3e320d5fecea1
Tedo <teddy2@abv.bg>
2020-02-16 22:30:42 +0200

    Register form validations

--- advertisements/src/app/auth/register/register.component.ts
+++ advertisements/src/app/auth/register/register.component.ts
@@ -8,13 +8,29 @@ import { NgForm } from '@angular/forms';
 	styleUrls: [ './register.component.css' ]
 })
 export class RegisterComponent implements OnInit {
+	passCheck = false;
 	constructor(private authServ: AuthService) {}
 
 	ngOnInit() {}
 
 	onReg(f: NgForm) {
-		let email = f.value.email;
-		let pass = f.value.password;
-		this.authServ.register(email, pass);
+		this.check(f.value.password, f.value.passConfirm);
+		if (!this.passCheck) {
+			// console.log(f.value.password, f.value.passConfirm, 'not mach');
+			return;
+		} else {
+			let email = f.value.email;
+			let pass = f.value.password;
+			this.authServ.register(email, pass);
+		}
+	}
+
+	check(pass: string, passConfirm: string) {
+		if (pass === passConfirm) {
+			this.passCheck = true;
+		} else {
+			this.passCheck = false;
+		}
+		// console.log(pass, passConfirm, this.passCheck);
 	}
 }
